9e4428afb282ba306e17a9acb72fecef7e31c63c
Silas Maciel Ferreira <s2it_sferreira@uolinc.com>
2018-04-13 16:36:08 -0300

    Implementação insertion sort

--- src/main/java/com/github/siilas/apa/service/InsertionSort.java
+++ src/main/java/com/github/siilas/apa/service/InsertionSort.java
@@ -9,6 +9,15 @@ public class InsertionSort implements Sortable {
 
     @Override
     public Sorted sort(Integer[] vetor) {
+        for (int i = 1; i < vetor.length; i++) {
+            int chave = vetor[i];
+            int aux = i - 1;
+            while (aux >= 0 && chave < vetor[aux]) {
+                vetor[aux + 1] = vetor[aux];
+                vetor[aux] = chave;
+                aux = aux - 1;
+            }
+        }
         return null;
     }
 
