013a5a29c2dde451b60a1806e5c69e501cc0d07f
Silas Maciel Ferreira <s2it_sferreira@uolinc.com>
2018-05-02 18:06:18 -0300

    Correção no quick sort

--- src/main/java/com/github/siilas/apa/service/QuickSortPivoPrimeiro.java
+++ src/main/java/com/github/siilas/apa/service/QuickSortPivoPrimeiro.java
@@ -9,6 +9,7 @@ public class QuickSortPivoPrimeiro implements Sortable {
 
     @Override
     public Integer[] specificSort(Integer[] vetor) {
+        //Evitando estouro da pilha
         if (vetor.length <= 10000) {
             ordena(vetor, 0, (vetor.length - 1));
         }
